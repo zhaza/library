2b492728036262a86237fa3f18e3cab94aabb8fe
zhaza <Khail.ballin12@yahoo.com>
2022-06-26 19:00:47 -0700

    Pushing for latest code. Asking for help on Discord

--- app.js
+++ app.js
@@ -1,52 +1,48 @@
+// set storage
 let myLibrary = [];
 
+// book function
 function Book(author, title, pages, read) {
   this.author = author;
   this.title = title;
   this.pages = pages;
   this.read = read;
 }
-// function addBookToLibrary(book) {
-//   myLibrary.push(Book());
-// }
+
+function addBookToLibrary() {
+  const author = document.getElementById("author").value;
+  const title = document.getElementById("title").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("readYes").value;
+  const add = new Book(author, title, pages, read);
+  myLibrary.push(add);
+}
 
 function printLibrary() {
   for (let i = 0; i < myLibrary.length; i++) {
-    const card = document.createElement("p");
-    card.classList.add("card");
-    card.textContent = myLibrary[i];
+    const container = document.getElementById("libraryShelf");
+    let card = document.createElement('div');
+    card.id = myLibrary[i].title;
+    card.innerHTML = `
+    <div>
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">${myLibrary[i].title}</h5>
+        <p class="card-text">Author: ${myLibrary[i].author}</p>
+        <p class="card-text"># Pages: ${myLibrary[i].pages}</p>
+        <p class="card-text">Read: ${myLibrary[i].read}</p>
+        <a href="#" class="btn btn-warning">Delete</a>
+      </div>
+    </div>
+    </div>`
+  ;
     container.appendChild(card);
   }
 }
-// printLibrary();
 
-function createForm() {
-  const container = document.querySelector("container");
-  const div = document.createElement("div");
-  div.innerHTML = `<h1>hello</h1>`;
-  container.appendChild(div);
-}
-
-const button = document.getElementById("newBook");
-button.addEventListener("click", createForm);
-// <form action="" method="get" class="">
-//     <div class="">
-//         <label for="author">Author: </label>
-//         <input type="text" name="author" id="author" required>
-//     </div>
-//     <div class="">
-//         <label for="author">Author: </label>
-//         <input type="text" name="author" id="author" required>
-//     </div>
-//     <div class="">
-//         <label for="author">Author: </label>
-//         <input type="text" name="author" id="author" required>
-//     </div>
-//     <div class="">
-//         <label for="author">Author: </label>
-//         <input type="radio" name="" id="author" required>
-//     </div>
-//     <div class="">
-//         <input type="submit" value="Subscribe!">
-//     </div>
-// </form>
+const form = document.getElementById("bookForm");
+form.addEventListener("submit", function(event) {
+  addBookToLibrary();
+  printLibrary();
+  event.preventDefault;
+});
